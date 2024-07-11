7b2410dde1898183af47e2059cc1af8f74118bf7
Johnny Herrera <jmjaramillo95@gmail.com>
2024-07-10 23:23:03 -0500

    veterinaria-react

---
+++ src/hooks/useCrud.js
@@ -0,0 +1,50 @@
+import axios from "axios";
+import { useState } from "react";
+
+
+const useCrud = (urlBase) => {
+  
+    const [apiData, setapiData] = useState();
+
+    //read
+    const getApi = (path) => {
+        axios.get(`${urlBase}${path}/`)
+        .then(res => setapiData(res.data))
+        .catch(err = console.log(err))
+    }
+
+    //create
+    const postApi = (path, data) => {
+        axios.post(`${urlBase}${path}/`, data)
+            .then(res => {
+                setapiData([...apiData, res.data]);
+                console.log(res.data);
+            })
+            .catch(err => console.log(err))
+    }
+
+    const deleteApi = (path, id) => {
+        axios.delete(`${urlBase}${path}/${id}/`)
+            .then(() => {
+                setapiData(apiData.filter(element => element.id !== id))
+                console.log('borrado con exito')
+            })
+            .catch()
+    }
+
+    //update
+    const updateApi = (path, id, data) => {
+        axios.patch(`${urlBase}${path}/${id}/`, data)
+            .then(res => {
+                setapiData(apiData.map(element => element.id === id ? res.data : element));
+                console.log(res.data)
+            })
+            .catch(err => console.log(err));
+    }
+
+    return [apiData, getApi, postApi, deleteApi, updateApi];
+
+
+}
+
+export default useCrud;

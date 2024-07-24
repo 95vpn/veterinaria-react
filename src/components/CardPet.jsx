09f7bf00249f5e4a74f5f353e8b3a10a40195352
Johnny Herrera <jmjaramillo95@gmail.com>
2024-07-23 23:58:22 -0500

    pdf-listo

--- src/components/CardPet.jsx
+++ src/components/CardPet.jsx
@@ -18,22 +18,23 @@ const CardPet = ({ pet, deletePets, setEditUser, setIsOpen }) => {
     return (
         <article className='container__card__info'>
             <h3>{pet.owner_name} {pet.owner_lastName}</h3>
-            <div>
+            <div className='container__card__info__list'>
 
-                <ul className='container__card__owner'>Owner Information
+                <ul className='container__card__owner'>
+                    <h4>Owner Information</h4>
                     <li><span>Address:  </span><span>{pet.address}</span></li>
                     <li><span>Phone:  </span><span>{pet.phone}</span></li>
                     <li><span>Email:  </span><span>{pet.email}</span></li>
                     <li><span>Date:  </span><span>{pet.release_data}</span></li>
                 </ul>
 
-                <ul className='container__card__pet'>Pet Information
+                <ul className='container__card__pet'>
+                    <h4>Pet Information</h4>
                     <li><span>Name:  </span><span>{pet.name}</span></li>
                     <li><span>Species:  </span><span>{pet.species}</span></li>
                     <li><span>Breed:  </span><span>{pet.breed}</span></li>
                     <li><span>Age:  </span><span>{pet.age}</span></li>
                     <li><span>Weight:  </span><span>{pet.weigth}</span></li>
-                    <li><span>Vaccinated:  </span><span>{pet.vaccinated}</span></li>
                     <li><span>Vaccinated:  </span><span>{pet.vaccinated}</span></li>
                     <li><span>Anamnesicos:  </span><span>{pet.anamnesicos}</span></li>
                 </ul>
--- src/components/PetPDF.jsx
+++ src/components/PetPDF.jsx
@@ -1,51 +1,78 @@
 import React from 'react';
 import { Document, Page, Text, View, StyleSheet} from '@react-pdf/renderer';
 import CardPet from './CardPet';
+// import logo from "../../public/logo-pet.PNG";
 
 const styles = StyleSheet.create({
-    page: {
-      flexDirection: 'column',
-      padding: 30,
-    },
-    section: {
-      
-      
-    },
-    title: {
-      fontSize: 24,
-      textAlign: 'center',
-    },
-    text: {
-      fontSize: 12,
-      margin: 2,
-    },
-    bold: {
-        fontWeight: 'bold',
-      }
+  page: {
+    flexDirection: 'column',
+    padding: 30,
+},
+header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 20,
+},
+logo: {
+    width: 60,
+    height: 60,
+},
+headerText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+},
+section: {
+    marginBottom: 20,
+},
+title: {
+    fontSize: 20,
+    marginBottom: 10,
+},
+text: {
+    fontSize: 12,
+    margin: 2,
+},
+bold: {
+    fontWeight: 'bold',
+},
+footer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    position: 'absolute',
+    bottom: 30,
+    left: 30,
+    right: 30,
+    fontSize: 10,
+},
   });
 
 const PetPDF = ({ pet }) => {
     return (
         <Document>
         <Page size="A4" style={styles.page}>
+          <View>
+          {/* <Image src={logo}/> */}
+          <Text style={styles.headerText}>Nombre de la Veterinaria</Text>
+          <Text style={styles.headerText}>"Cuidamos a tus mascotas como a nuestra familia"</Text>
+          </View>
           <View style={styles.section}>
             <Text style={styles.title}>Owner Information</Text>
-            <Text style={styles.text}><Text style={styles.bold} >Owner Name: </Text> {pet.owner_name} {pet.owner_lastName}</Text>
-            <Text style={styles.text}><Text style={styles.bold} >Address: </Text>{pet.address}</Text>
-            <Text style={styles.text}><Text style={styles.bold} >Phone: </Text>{pet.phone}</Text>
-            <Text style={styles.text}><Text style={styles.bold} >Email: </Text>{pet.email}</Text>
-            <Text style={styles.text}><Text style={styles.bold} >Date: </Text>{pet.release_data}</Text>
+            <Text style={styles.text}><Text style={styles.bold} >Owner Name:  </Text> {pet.owner_name} {pet.owner_lastName}</Text>
+            <Text style={styles.text}><Text style={styles.bold} >Address:  </Text>{pet.address}</Text>
+            <Text style={styles.text}><Text style={styles.bold} >Phone:  </Text>{pet.phone}</Text>
+            <Text style={styles.text}><Text style={styles.bold} >Email:  </Text>{pet.email}</Text>
+            <Text style={styles.text}><Text style={styles.bold} >Date:  </Text>{pet.release_data}</Text>
           </View>
           <View style={styles.section}>
             <Text style={styles.title}>Pet Information</Text>
-            <Text style={styles.text}><Text style={styles.bold} >Name:</Text>{pet.name}</Text>
-            <Text style={styles.text}><Text style={styles.bold} >Species:</Text>{pet.species}</Text>
-            <Text style={styles.text}><Text style={styles.bold} >Breed:</Text>{pet.breed}</Text>
-            <Text style={styles.text}><Text style={styles.bold} >Age: </Text>{pet.age}</Text>
-            <Text style={styles.text}><Text style={styles.bold} >Weight:</Text>{pet.weigth}</Text>
-            <Text style={styles.text}><Text style={styles.bold} >Vaccinated: </Text>{pet.vaccinated}</Text>
-            <Text style={styles.text}><Text style={styles.bold} >Anamnesicos:</Text>{pet.anamnesicos}</Text>
-            <Text style={styles.bold} >Anamnesicos:</Text>
+            <Text style={styles.text}><Text style={styles.bold} >Name:  </Text>{pet.name}</Text>
+            <Text style={styles.text}><Text style={styles.bold} >Species:  </Text>{pet.species}</Text>
+            <Text style={styles.text}><Text style={styles.bold} >Breed:  </Text>{pet.breed}</Text>
+            <Text style={styles.text}><Text style={styles.bold} >Age:  </Text>{pet.age}</Text>
+            <Text style={styles.text}><Text style={styles.bold} >Weight:  </Text>{pet.weigth}</Text>
+            <Text style={styles.text}><Text style={styles.bold} >Vaccinated:  </Text>{pet.vaccinated}</Text>
+            <Text style={styles.text}><Text style={styles.bold} >Anamnesicos:  </Text>{pet.anamnesicos}</Text>
           </View>
         </Page>
       </Document>
